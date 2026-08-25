0025af2b22a291d652208086f7ba3d796136b9e9
Tamim <tamimhossen112@gmail.com>
2026-08-25 14:15:55 +0600

    Initial commit: Multi-Vendor E-Commerce Complete Core Core Core Flow

--- backend/settings.py
+++ backend/settings.py
@@ -159,6 +159,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # হোয়াইটস্পেস অপ্টিমাইজেশন সেটিংস
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage", # 🔥 এটি ইমেজের জন্য বাধ্যতামূলক
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
