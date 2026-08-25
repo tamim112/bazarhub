8efb54ad83794598010185dab3d4b9f58eba587b
Tamim <tamimhossen112@gmail.com>
2026-08-25 12:43:37 +0600

    Fix: Configured WhiteNoise for live Django Admin UI static files

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,6 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # 🔥 লাইভ সার্ভারে স্ট্যাটিক ফাইল জমার মেইন ফোল্ডার
+
+# হোয়াইটস্পেস অপ্টিমাইজেশন সেটিংস
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
